208d4d6a42f9439fbffbcc1c9db9006f1439080a
paolocrosetto <paolo.crosetto@gmail.com>
2024-11-18 12:49:13 +0100

    Setup to include Study 1: data and changes ot main script

--- Analysis.R
+++ Analysis.R
@@ -8,7 +8,7 @@
 ## This version: 0.1, 11 Nov 2024
 ##
 ##
-## This file replicates the figures and tables in the main text of the paper
+## This file replicates the figures and tables in the main text and the supplement of the paper
 
 #### 0. Libraries ####
 
@@ -19,18 +19,36 @@ library(patchwork)        # combine ggplots into one plot
 
 library(kableExtra)       # nice-looking table export
 
-#### 1. Data ####
+
+#### 0. Main text ####
+
+#### 0.1 Data ####
 
 df <- read_csv("Data/all_studies_clean.csv")
 
 
-
-#### 2. Figure 1 ####
+#### 0.2 Figure 1 ####
 
 source("Scripts/Figure_1.R")
 
 
-#### 3. Table 1 ####
+#### 0.3 Table 1 ####
 
 source("Scripts/Table_1.R")
+
+
+##### 1. Supplementary material: Study 1 ####
+
+##### 1.1 Data ####
+
+s1 <- read_csv("Data/study_1.csv")
+
+#### 1.2 Tables 
+
+## TODO Table S1
+## TODO Table S2
+## TODO Table S3
+## TODO Table S4
+
+
 
